d46d6f936739e42c091a19540e07284b751e8c30
Steven Meehan <smeehan82@gmail.com>
2016-02-20 20:38:10 -0500

    Added Attributes for data mapping

---
+++ src/Hermes/DataAnnotations/IgnorePropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnorePropertyAttribute : Attribute
+    {
+        public IgnorePropertyAttribute()
+        {
+        }
+    }
+}
---
+++ src/Hermes/DataAnnotations/PrimaryKeyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+        public string PrimaryKeyName { get; set; }
+
+        public PrimaryKeyAttribute()
+        {
+        }
+    }
+}
