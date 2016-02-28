d69861a5941477190824a16b8ae859f7ce04cc0e
Steven Meehan <smeehan82@gmail.com>
2016-02-28 11:49:06 -0500

    added the PersistentItemManager

---
+++ src/Hermes/DataAccess/IPersistentItemManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAccess
+{
+    public interface IPersistentItemManager<TItem, TKey>
+        where TItem : class, IPersistentItem<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        Task<HermesResult> AddAsync(TItem item);
+        IQueryable<TItem> Items { get; }
+        Task<TItem> FindByIdAsync(TKey id);
+        Task<HermesResult> UpdateAsync(TItem item);
+        Task<HermesResult> DeleteByIdAsync(TKey id);
+        Task<HermesResult> DeleteAsync(TItem item);
+    }
+}
