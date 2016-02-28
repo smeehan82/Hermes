d6408b9a29aa0fb69791046a5aa90102d1216e40
Steven Meehan <smeehan82@gmail.com>
2016-02-28 13:11:45 -0500

    added an interface for the blogs manager

---
+++ src/Hermes.Content.Blogs/IBlogsManager.cs
@@ -0,0 +1,14 @@
+﻿using Hermes.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.Content.Blogs
+{
+    public interface IBlogsManager : IContentManager<Blog, Guid>
+    {
+        IQueryable<Blog> Blogs { get; }
+        IQueryable<BlogPost> BlogPosts { get; }
+    }
+}
