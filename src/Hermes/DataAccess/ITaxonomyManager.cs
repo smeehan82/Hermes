37897d4daeabe86b0cd0f0641b97e751dd9ae63c
Steven Meehan <smeehan82@gmail.com>
2016-02-28 16:13:00 -0500

    added a taxonomy manager

---
+++ src/Hermes/DataAccess/ITaxonomyManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAccess
+{
+    public interface ITaxonomyManager<TTaxonomy, TKey> : IPersistentItemManager<TTaxonomy, TKey>
+        where TTaxonomy : class, ITaxonomy<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        Task<TTaxonomy> FindBySlugAsync(string slug);
+
+        Task<string> GenerateNewSlug(string source);
+        Task<string> NormalizeSlug(string source);
+    }
+}
