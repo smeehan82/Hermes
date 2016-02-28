6c1dcbe33616b17632c05a92ef839e8b9ca81403
Steven Meehan <smeehan82@gmail.com>
2016-02-28 11:42:26 -0500

    Added results to the Core Project

---
+++ src/Hermes/HermesErrorDescriber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes
+{
+    public class HermesErrorDescriber
+    {
+        public virtual HermesError DefaultError()
+        {
+            return new HermesError
+            {
+                Code = nameof(DefaultError),
+                Description = "This is a Hermes Default Error"
+            };
+        }
+
+        public virtual HermesError ConcurrencyFailure()
+        {
+            return new HermesError
+            {
+                Code = nameof(ConcurrencyFailure),
+                Description = "Optimistic concurrency failure, object has been modified."
+            };
+        }
+
+        //Titles Not present
+
+    }
+}
