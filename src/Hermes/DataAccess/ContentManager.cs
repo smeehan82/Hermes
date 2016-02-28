9981555bdf947850ebd4ba33adf30ac2b9eae1b8
Steven Meehan <smeehan82@gmail.com>
2016-02-28 11:49:33 -0500

    added the ContentManager

---
+++ src/Hermes/DataAccess/ContentManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Http;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAccess
+{
+    public abstract class ContentManager<TContent, TKey> : PersistentItemManager<TContent, TKey>, IContentManager<TContent, TKey>
+        where TContent : class, IContent<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public ContentManager(IContentStore<TContent, TKey> store, IHttpContextAccessor contextAccessor) : base(store, contextAccessor) { }
+
+        public async Task<TContent> FindBySlugAsync(string slug)
+        {
+            return await _store.Items.FirstOrDefaultAsync(c => c.Slug.Equals(slug), CancellationToken);
+        }
+
+
+        //**********Helper Methods**********//
+        //**********************************//
+        //**********************************//
+        public virtual Task<string> GenerateNewSlug(string source)
+        {
+            //@TODO make it international friendly and cleanup the regex and remove small words
+            var slug = Regex.Replace(source, @"\s+", "-");
+            return Task.FromResult(Regex.Replace(slug.Normalize().ToLowerInvariant(), @"[^\d\w]", "-"));
+        }
+
+        public virtual Task<string> NormalizeSlug(string source)
+        {
+            return Task.FromResult(source.Normalize().ToLowerInvariant());
+        }
+    }
+}
---
+++ src/Hermes/DataAccess/IContentManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAccess
+{
+    interface IContentManager<TContent, TKey> : IPersistentItemManager<TContent, TKey>
+        where TContent : class, IContent<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        Task<TContent> FindBySlugAsync(string slug);
+
+        Task<string> GenerateNewSlug(string source);
+        Task<string> NormalizeSlug(string source);
+    }
+}
