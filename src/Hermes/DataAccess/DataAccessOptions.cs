f7f4d6601cfdb7b4b5df9766d1a64e3d34d76925
Steven Meehan <smeehan82@gmail.com>
2016-02-28 07:58:15 -0500

    misc update to the data access options

---
+++ src/Hermes/DataAccess/DataAccessOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.DataAccess
+{
+    public class DataAccessOptions
+    {
+        public DataConnectionOptions DefaultConnection { get; set; } = new DataConnectionOptions();
+    }
+
+    public class DataConnectionOptions    {        public DatabaseType Type { get; set; } = DatabaseType.MsSqlServer;        public string ConntectionString { get; set; }    }    public enum DatabaseType    {        PostgreSql,        MsSqlServer    }
+
+}
