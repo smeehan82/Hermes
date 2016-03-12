fcbc0dd9f390380e8391235fb31b65e9c74fefb9
Steven Meehan <smeehan82@gmail.com>
2016-03-12 11:55:30 -0500

    added the files to the Hermes Pages namespace

---
+++ src/Hermes/Pages/IPageContent.cs
@@ -0,0 +1,18 @@
+﻿using Hermes.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.Pages
+{
+    public interface IPageContent<TKey> : IPersistentItem<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        //IPage Page { get; set; }
+        Guid PageId { get; set; }
+        string Paramaters { get; set; }
+    }
+
+    public interface IPageContent : IPageContent<Guid> { }
+}
---
+++ src/Hermes/Pages/Widget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.Pages
+{
+    public abstract class Widget : IWidget
+    {
+        public string ConcurrencyStamp { get; set; }
+        public Guid Id { get; set; }
+        public Guid PageId { get; set; }
+        public string Paramaters { get; set; }
+    }
+}
