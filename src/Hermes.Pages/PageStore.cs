1fab08242f12e95706f40c70dabc736eecb76cac
Steven Meehan <smeehan82@gmail.com>
2016-03-12 11:57:51 -0500

    Added the Pages project

---
+++ src/Hermes.Pages/PageStore.cs
@@ -0,0 +1,28 @@
+﻿using Hermes.DataAccess;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.Pages
+{
+    public interface IPageStore : IPersisteneItemStore<Page, Guid>
+    {
+        IQueryable<Page> Pages { get; }
+    }
+
+    public class PageStore : PersistentItemStore<Page>, IPageStore
+    {
+        #region Contructor
+
+        public PageStore(IDataContext context, ILoggerFactory loggerFactory) : base(context)
+        {
+            _logger = loggerFactory.CreateLogger<PageStore>();
+        }
+
+        #endregion
+
+        public IQueryable<Page> Pages { get { return Items; } }
+    }
+}
---
+++ src/Hermes.Pages/PlainTextPageContent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Hermes.DataAccess;
+
+namespace Hermes.Pages
+{
+    public class PlainTextPageContent : IPageContent, IPersistentItem
+    {
+        public string ConcurrencyStamp
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Guid Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+              
+        public Guid PageId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Paramaters
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
