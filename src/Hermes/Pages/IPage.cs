0f2a59637ef40a0ce5eb345a48375d592b7d7254
Steven Meehan <smeehan82@gmail.com>
2016-03-12 11:51:45 -0500

    moved IPage to the Pages namespace

---
+++ src/Hermes/Pages/IPage.cs
@@ -0,0 +1,20 @@
+﻿using Hermes.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hermes.Pages
+{
+    public interface IPage<TKey> : IPersistentItem<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        //ITemplate PageTemplate { get; set; }
+        //IPageContent PageContent { get; set; }
+        //ICollection<IWidget> PageWidgets { get; set; }
+        string Name { get; set; }
+        string Path { get; set; }
+    }
+
+    public interface IPage : IPage<Guid> { }
+}
